c76c803126abc8d8ae1abf001aff91cabb4fc025
oDoctorDoom <60649628+oDoctorDoom@users.noreply.github.com>
2021-03-11 15:36:42 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,24 @@
-<?php
-/*POST /{ig-user-id}/media?image_url={image-url}
+<html>
 
-POST graph.facebook.com/17841400008460056/media
-  ?image_url=https//www.example.com/images/bronzed-fonzes.jpg
-  &caption=#BronzedFonzes!
-  &user_tags=
-    [
-      {
-        username:'toDoctorDoom',
-        x: 0.5,
-        y: 0.8
-      },
-      {
-        username:'toDoctorDoom',
-        x: 0.3,
-        y: 0.2
-      }
-    ]
-?>
-*/?>
-<script>
+  <script>
   window.fbAsyncInit = function() {
     FB.init({
-      appId            : '514488396201603',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v10.0'
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
     });
+      
+    FB.AppEvents.logPageView();   
+      
   };
-</script>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
-<html>
-GET /v10.0/{instagram-media-id} HTTP/1.1
-Host: graph.facebook.com
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 </html>
